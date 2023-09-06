b49c884a29fabbf8a36d7dedb25d4a02addf8e03
Bhargav Borse <bhargav.borse4@gmail.com>
2023-09-06 10:47:15 -0400

    files added

---
+++ WebTech-712/Lab-1/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About me</Text>
+      <Text>Bhargav Borse</Text>
+      <Text>Id: 301278352</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    fontSize: 40,
+  },
+});
