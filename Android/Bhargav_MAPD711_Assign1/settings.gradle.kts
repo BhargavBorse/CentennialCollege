210ffcf52e4f303940a521d8b81f201895ecfffd
Bhargav <bhargav.borse4@gmail.com>
2023-09-25 12:41:12 -0400

    added android project - assignment 1 files

---
+++ Android/Bhargav_MAPD711_Assign1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bhargav_MAPD711_Assign1"
+include(":app")
+ 
