200b7bf1de4c7fa2759fe4110d9f5ce229ac7173
Bhargav <bhargav.borse4@gmail.com>
2023-09-27 14:53:35 -0400

    updated design a bit

--- WebTech-712/Bhargav_301278352_BMI_Calculator/App.js
+++ WebTech-712/Bhargav_301278352_BMI_Calculator/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 
 const App = () => {
@@ -17,35 +17,47 @@ const App = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>BMI Calculator</Text>
-      <RNPickerSelect
-        placeholder={{
-          label: 'Select Measurement System',
-          value: null,
-        }}
-        items={[
-          { label: 'Metric (kg, cm)', value: 'metric' },
-          { label: 'Imperial (lb, in)', value: 'imperial' },
-        ]}
-        onValueChange={(value) => setMeasurementSystem(value)}
-        style={pickerSelectStyles}
-        value={measurementSystem}
-      />
-      <TextInput
-        placeholder="Weight (kg or lb)"
-        style={styles.input}
-        onChangeText={(text) => setWeight(text)}
-        value={weight}
-        keyboardType="numeric"
-      />
-      <TextInput
-        placeholder="Height (cm or in)"
-        style={styles.input}
-        onChangeText={(text) => setHeight(text)}
-        value={height}
-        keyboardType="numeric"
-      />
-      <Button title="Calculate BMI" onPress={calculateBMI} />
+      <View style={styles.header}>
+        <Text style={styles.title}>BMI Calculator</Text>
+        <RNPickerSelect
+          placeholder={{
+            label: 'Select Measurement System',
+            value: null,
+          }}
+          items={[
+            { label: 'Metric (kg, cm)', value: 'metric' },
+            { label: 'Imperial (lb, in)', value: 'imperial' },
+          ]}
+          onValueChange={(value) => setMeasurementSystem(value)}
+          style={pickerSelectStyles}
+          value={measurementSystem}
+        />
+      </View>
+      <View style={styles.content}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.inputLabel}>Weight</Text>
+          <TextInput
+            placeholder="e.g., 70"
+            style={styles.input}
+            onChangeText={(text) => setWeight(text)}
+            value={weight}
+            keyboardType="numeric"
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.inputLabel}>Height</Text>
+          <TextInput
+            placeholder="e.g., 170"
+            style={styles.input}
+            onChangeText={(text) => setHeight(text)}
+            value={height}
+            keyboardType="numeric"
+          />
+        </View>
+        <TouchableOpacity style={styles.calculateButton} onPress={calculateBMI}>
+          <Text style={styles.buttonText}>Calculate BMI</Text>
+        </TouchableOpacity>
+      </View>
       {bmi && (
         <View style={styles.resultContainer}>
           <Text style={styles.resultLabel}>Your BMI:</Text>
@@ -67,37 +79,62 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5', // Minimal background color
     padding: 20,
-    backgroundColor: '#f0f0f0',
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 20,
   },
   title: {
-    fontSize: 28,
+    fontSize: 32,
     fontWeight: 'bold',
-    marginBottom: 20,
+    color: '#333',
+  },
+  content: {
+    width: '100%',
+  },
+  inputGroup: {
+    marginBottom: 15,
+  },
+  inputLabel: {
+    fontSize: 18,
     color: '#333',
   },
   input: {
-    width: '100%',
     height: 50,
-    borderColor: '#ddd',
+    borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 8,
     paddingLeft: 15,
-    marginBottom: 15,
     fontSize: 18,
     backgroundColor: 'white',
+    color: '#333',
+  },
+  calculateButton: {
+    backgroundColor: '#3498db',
+    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
   },
   resultContainer: {
-    marginTop: 30,
     alignItems: 'center',
   },
   resultLabel: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
+    marginTop: 20,
   },
   resultValue: {
     fontSize: 48,
@@ -116,10 +153,9 @@ const pickerSelectStyles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 10,
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: '#ccc',
     borderRadius: 8,
-    color: 'black',
-    marginBottom: 15,
+    color: '#333',
     backgroundColor: 'white',
   },
   inputAndroid: {
@@ -127,10 +163,9 @@ const pickerSelectStyles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 8,
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: '#ccc',
     borderRadius: 8,
-    color: 'black',
-    marginBottom: 15,
+    color: '#333',
     backgroundColor: 'white',
   },
 });
