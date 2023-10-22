4224e050191e5b7f47d863e1c44331b8f758adc6
Bhargav <bhargav.borse4@gmail.com>
2023-10-22 11:02:24 -0400

    allPatients completed

--- WebTech-712/Project files/Group3_Webtech/src/navigation/AppNavigator.js
+++ WebTech-712/Project files/Group3_Webtech/src/navigation/AppNavigator.js
@@ -5,9 +5,9 @@ import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import MainPage from '../screens/MainPage';
+import AllPatients from '../screens/AllPatients';
 
 function AppNavigator() {
-
   const Stack = createStackNavigator();
 
   return (
@@ -17,6 +17,7 @@ function AppNavigator() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Main" component={MainPage} />
+        <Stack.Screen name="AllPatients" component={AllPatients} />
       </Stack.Navigator>
     </NavigationContainer>
   );
